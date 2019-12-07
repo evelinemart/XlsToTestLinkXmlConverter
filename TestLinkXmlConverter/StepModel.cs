2f5340fef4d6bfe9128ad7e7455cf8986f849ec9
Mariia Khrapunova <mariia.khrapunova@techs.com.ua>
2019-12-07 12:23:29 +0200

    Add project files.

---
+++ TestLinkXmlConverter/StepModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TestLinkXmlConverter
+{
+    public class StepModel
+    {
+        public int step_number { get; set; }
+        public string actions { get; set; }
+        public string expectedresults { get; set; }
+        public int execution_type { get; set; }
+    }
+}
